d1461dc961427b68060edccda75cfb15f3e22e93
Mayank Soni <sonimayank151@gmail.com>
2025-12-07 00:35:09 +0530

    debugged signup page

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -20,7 +20,7 @@ export default function SignupPage() {
     try {
       setLoading(true);
       const response = await axios.post("api/users/signup", user);
-
+      toast.success("Signup Successful");
       console.log("Signup success", response.data);
       router.push("/login");
     } catch (error: any) {

3357a7829b2e5c0c116555e3a2d5364e5b3d571a
Mayank Soni <sonimayank151@gmail.com>
2025-12-06 23:55:14 +0530

    getting user details

--- src/app/api/users/me/route.ts
+++ src/app/api/users/me/route.ts
@@ -3,6 +3,9 @@ import { getDataFromToken } from "@/helpers/getDataFromToken";
 import { NextRequest, NextResponse } from "next/server";
 import User from "@/models/userModel";
 
+import { connect } from "@/dbConfig/dbConfig";
+await connect(); 
+
 export async function GET(request: NextRequest) {
     try {
         const userId = getDataFromToken(request);
